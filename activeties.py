017d8941eed8c991b227f9a96cfbe002ea8ab7f0
Ylgner Becton <ylgner.becton@gmail.com>
2017-09-29 12:48:20 -0300

    activities

---
+++ activeties.py
@@ -0,0 +1,104 @@
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.1
+x=1
+while x<=100:
+    print(x)
+    x = x + 1
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.2
+x=50
+while x<=100:
+    print(x)
+    x = x + 1
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.3
+x=10
+while x>=0:
+    print(x)
+    x = x - 1
+print("Fogo!")
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.4
+fim=int(input("Digite o último número a imprimir:"))
+x = 0
+while x <= fim:
+    print(x)
+    x = x + 3
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.5
+x = 10
+while x <= 30:
+    print(x)
+    x = x + 3
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.6
+n = int(input("Tabuada de:"))
+x = 1
+while x <= 10:
+    print("%d x %d = %d" % (n, x, n*x) )
+    x = x + 1
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.7
+n = int(input("Tabuada de:"))
+inicio = int(input("De:"))
+fim = int(input("Até:"))
+x = inicio
+while x <= fim:
+    print("%d x %d = %d" % (n, x, n*x) )
+    x = x + 1
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.8
+p = int(input("Primeiro número:"))
+s = int(input("Segundo número:"))
+x = 1
+r = 0
+while x <= s:
+    r = r + p
+    x = x + 1
+print("%d x %d = %d" % (p,s,r)
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.9
+dividendo = int(input("Dividendo:"))
+divisor = int(input("Divisor:"))
+quociente = 0
+x = dividendo
+while x >= divisor:
+    x = x - divisor
+    quociente = quociente + 1
+resto = x
+print("%d / %d = %d (quociente) %d (resto)" % (dividendo,divisor,quociente,resto))
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.10
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.11
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.12
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.13
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.14
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.15
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.16
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.17
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.18
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.19
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.20
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.21
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.22
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.23
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.24
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.25
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.26
+
+# INT. PROG. PYTHON CAP. 5 | EXERCÍCIO 5.27
